31627fe14dd0892a4a5a83e6f5ae5489ad2ea6ea
Peter <peter@vicedigital.co.za>
2018-01-19 18:47:03 +0200

    Updated Exceptions

--- src/Router/Exceptions/InvalidParameterMatchException.php
+++ src/Router/Exceptions/InvalidParameterMatchException.php
@@ -3,30 +3,88 @@ namespace Kit\Http\Router\Exceptions;
 
 use Exception;
 use App\BaseException;
+use App\Exceptions\Contract\ExceptionContract;
 
-class InvalidParameterMatchException extends BaseException
+class InvalidParameterMatchException extends BaseException implements ExceptionContract
 {
-
-	/**
-	* @var 		$template
-	* @access 	public
-	*/
-	public 		$template = '404x';
 
 	/**
 	* @var 		$code
 	* @access 	protected
 	*/
-	protected 	$code = 500;
+	protected 	$code = 404;
 
 	/**
-	* @param 	$message <String>
+	* @var 		$message
+	* @access 	protected
+	*/
+	protected 	$message;
+
+	/**
+	* @var 		$view
+	* @access 	protected
+	*/
+	protected 	$view;
+
+	/**
+	* @param 	$options <Array>
 	* @access 	public
 	* @return 	void
 	*/
-	public function __construct($message='')
+	public function __construct(...$options)
 	{
-		parent::__construct($message);
+		$this->setCode(404);
+		$this->setView('exception');
+		$this->setMessage($options[0]);
+		parent::__construct();
+	}
+
+	/**
+	* {@inheritDoc}
+	*/
+	public function setCode(int $code)
+	{
+		$this->code = $code;
+	}
+
+	/**
+	* {@inheritDoc}
+	*/
+	public function setMessage(String $message)
+	{
+		$this->message = $message;
+	}
+
+	/**
+	* {@inheritDoc}
+	*/
+	public function setView(String $view)
+	{
+		$this->view = $view;
+	}
+
+	/**
+	* {@inheritDoc}
+	*/
+	public function getExceptionCode() : int
+	{
+		return $this->code;
+	}
+
+	/**
+	* {@inheritDoc}
+	*/
+	public function getExceptionMessage() : String
+	{
+		return $this->message;
+	}
+
+	/**
+	* {@inheritDoc}
+	*/
+	public function getView() : String
+	{
+		return $this->view;
 	}
 
 }
